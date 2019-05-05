3385ecb86cd19e40db1cd82c6bc468d181a36caf
Michael Street <mi.street@gmail.com>
2019-05-04 23:17:42 -0400

    adding build to serve from github

---
+++ build/precache-manifest.97e95c0dbea3fa2ca29c7db5e2eafead.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b0e3fd429ad482360e86e83ddc6842",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce71d5908a5629dedc0",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "e2a6388b1c246059c445",
+    "url": "/static/js/2.1d7b4787.chunk.js"
+  },
+  {
+    "revision": "bce71d5908a5629dedc0",
+    "url": "/static/js/main.5312f06a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
